825aff6c36a34347eb413416196e662298fe5410
ExoticcYT <approxap@gmail.com>
2026-08-29 18:45:01 -0400

    the basic frontend is complete. Just have to link frontend with the backend using FastAPI, add some pleasing details to the UI, and enhance the UX. Will use RAG to add a chatbot to the program. The finish line is close.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,69 +1,185 @@
-import Image from "next/image";
+"use client";
+
+import {useState} from "react";
+
+function Dropdown({ label_name, dropdown_elements }: {
+  label_name: string;
+  dropdown_elements: string[];
+}){
+  return(
+    <div className="mt-6">
+      <label className="block text-lg font-medium">
+        {label_name}
+      </label>
+
+      <select className="mt-2 w-full rounded-lg bg-zinc-800 p-3 text-lg">
+        {dropdown_elements.map((element) => (
+          <option value={element} key={element}>{element}</option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+function NumInput({ label_name, placeholder, value, onChange }:{
+  label_name: string;
+  placeholder: string;
+  value: string;
+  onChange: (value: string) => void;
+}){
+  return(
+    <div className="mt-6">
+      <label className="block text-lg font-medium">
+        {label_name}
+      </label>
+      <input 
+        type="number" 
+        placeholder={placeholder} 
+        value = {value}
+        onChange = {(e) => onChange(e.target.value)}
+        className="mt-2 w-full rounded-lg bg-zinc-800 p-3 text-lg"
+      />
+    </div>
+  );
+}
 
 export default function Home() {
+  const [hoursStudied, setHoursStudied] = useState("");
+  const [attendancePercentage, setAttendancePercentage] = useState("");
+  const [avgSleep, setAvgSleep] = useState("");
+  const [avgScores, setAvgScores] = useState("");
+  const [tutoringSeshes, setTutoringSeshes] = useState("");
+  const [avgPE, setAvgPE] = useState("");
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+  <main className="min-h-screen bg-zinc-950 text-white p-10">
+    <div className="max-w-8xl mx-auto">
+      <h1 className="text-5xl font-bold">
+        Student Performance AI
+      </h1>
+
+      <p className="mt-2 text-zinc-400 text-lg">
+        Predict your exam score and get personalized study advice.
+      </p>
+
+      <section className="mt-8 rounded-2xl bg-zinc-900 p-6 pb-12">
+        <h2 className="text-3xl font-semibold">
+          Student Profile
+        </h2>
+
+        <div className="grid grid-cols-4 gap-6 mt-6">
+          <Dropdown
+            label_name="Parental Involvement"
+            dropdown_elements={["Low", "Medium", "High"]}
+          />
+          
+          <Dropdown
+            label_name="Access to Resources"
+            dropdown_elements={["Low", "Medium", "High"]}
+          />
+
+          <Dropdown
+            label_name="Motivation Level"
+            dropdown_elements={["Low", "Medium", "High"]}
+          />
+
+          <Dropdown
+            label_name="Family Income"
+            dropdown_elements={["Low", "Medium", "High"]}
+          />
+
+          <Dropdown
+            label_name="Teacher Quality"
+            dropdown_elements={["Low", "Medium", "High"]}
+          />
+
+          <Dropdown
+            label_name="Peer Influence"
+            dropdown_elements={["Negative", "Neutral", "Positive"]}
+          />
+
+          <Dropdown
+            label_name="Parent Education Level"
+            dropdown_elements={["High School", "College", "Postgraduate"]}
+          />
+
+          <Dropdown
+            label_name="Distance from Home"
+            dropdown_elements={["Near", "Moderate", "Far"]}
+          />
+
+          <Dropdown
+            label_name="Extracurricular Activities"
+            dropdown_elements={["Yes", "No"]}
+          />
+            
+          <Dropdown
+            label_name="Internet Access"
+            dropdown_elements={["Yes", "No"]}
+          />
+            
+          <Dropdown
+            label_name="School Type"
+            dropdown_elements={["Public", "Private"]}
+          />
+            
+          <Dropdown
+            label_name="Learning Disabilities"
+            dropdown_elements={["Yes", "No"]}
+          />
+            
+          <Dropdown
+            label_name="Gender"
+            dropdown_elements={["Male", "Female"]}
+          />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+      </section>
+      <section className="mt-8 rounded-2xl bg-zinc-900 p-6 pb-12">
+        <h2 className="text-3xl font-semibold">
+          Student Metrics
+        </h2>
+
+        <div className="grid grid-cols-3 gap-6 mt-6">
+          <NumInput 
+            label_name="Hours Studied" 
+            placeholder="Enter number of hours you study per week on average" 
+            value={hoursStudied}
+            onChange={setHoursStudied}
+          />
+          <NumInput 
+            label_name="Attendance" 
+            placeholder="Enter your average attendance % (0-100)"
+            value={attendancePercentage}
+            onChange={setAttendancePercentage}
+          />
+          <NumInput 
+            label_name="Sleep Hours" 
+            placeholder="Enter average sleeping hours at night"
+            value={avgSleep}
+            onChange={setAvgSleep}
+          />
+          <NumInput 
+            label_name="Previous Scores" 
+            placeholder="Enter avg previous exams score (N/A? enter any value 0-100)"
+            value={avgScores}
+            onChange={setAvgScores}
+          />
+          <NumInput 
+            label_name="Tutoring Sessions" 
+            placeholder="Enter # of times you saw a tutor for this test?"
+            value={tutoringSeshes}
+            onChange={setTutoringSeshes}
+          />
+          <NumInput 
+            label_name="Physical Activity" 
+            placeholder="Enter average physical activity hours per week"
+            value={avgPE}
+            onChange={setAvgPE}
+          />
         </div>
-      </main>
+      </section>
     </div>
+  </main>
   );
 }
