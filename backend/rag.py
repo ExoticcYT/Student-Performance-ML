daf663a92f03fd76a09870a8ed21c69a8dd3dc04
ExoticcYT <approxap@gmail.com>
2026-09-02 19:53:56 -0400

    gemini has been incorporated. Time to use FastAPI again to make it frontend-ready (ts pmo, but it is what it is)

--- backend/rag.py
+++ backend/rag.py
@@ -1,8 +1,17 @@
 from pathlib import Path
 from sentence_transformers import SentenceTransformer
 import faiss
+from google import genai
+from dotenv import load_dotenv
+import os
 
-KNOWLEDGE_DIR = Path("knowledge")
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+client = genai.Client(api_key=gemini_api_key)
+
+KNOWLEDGE_DIR = Path(__file__).parent/"knowledge"
 
 def load_documents():
     documents = []
@@ -33,16 +42,24 @@ dimension = embeddings.shape[1] #384
 index = faiss.IndexFlatL2(dimension) #we need this to perform the search
 index.add(embeddings) #adding the matrix to the index, which is actually a numpy array with 34 iterations of 1x384 arrays
 
-#just a test here
-query = "How can sleep affect my studying?"
-query_embedding = embedding_model.encode(query) 
-query_embedding = query_embedding.reshape(1, dimension) #end of process of making a 1x384 array from the query
-k=3
-distance, indices = index.search(query_embedding, k) #thats cuz this literally returns the distance and the indices, but the distance doesnt seem to bother us
+#the testing worked, now we actually code the reusability
+def generate_answer(query):
+    query_embedding = embedding_model.encode(query) 
+    query_embedding = query_embedding.reshape(1, dimension) #end of process of making a 1x384 array from the query
+    k=3
+    distance, indices = index.search(query_embedding, k) #thats cuz this literally returns the distance and the indices, but the distance doesnt seem to bother us
 
-context = []
-for indice in indices[0]:
-    doc = documents[indice]
-    context.append(doc['text'])
+    context = []
+    for indice in indices[0]:
+        doc = documents[indice]
+        context.append(doc['text'])
 
-context_string = "\n\n".join(context)
+    context_string = "\n\n".join(context)
+    contents = f"Context:\n{context_string}\n\nQuestion:\n{query}\n\nAnswer the question using the provided context."
+
+    response = client.models.generate_content(
+        model="gemini-3.7-flash",
+        contents=contents
+    )
+
+    return response.text
